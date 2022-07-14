4c7a5a670cbb5e12b1f026e593f67dba0de5f355
Majid Jabrayilov <cmecid@gmail.com>
2022-07-14 20:17:17 +0400

    Library name updated

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SwiftUnidirectionalFlow",
+            name: "UnidirectionalFlow",
             targets: ["UnidirectionalFlow"]),
     ],
     dependencies: [],
