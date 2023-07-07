7d62f303f11c2547b08684dc69e757ec5864cf64
Majid Jabrayilov <cmecid@gmail.com>
2023-07-07 22:42:06 +0400

    Swift Tools version bumped

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,6 @@ let package = Package(
     name: "swift-unidirectional-flow",
     platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16), .watchOS(.v9)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "UnidirectionalFlow",
             targets: ["UnidirectionalFlow"]),
